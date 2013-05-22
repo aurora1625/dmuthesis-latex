ed50ee1d07566a50a0d04cf8b49948810e1764d6
Zhibo Xiao <xiaozhibo@dlmu.edu.cn>
2013-05-22 21:21:31 +0800

    fix a font related error

--- run.bat
+++ run.bat
@@ -4,3 +4,5 @@ xelatex -no-pdf --interaction=nonstopmode dmuthesis
 bibtex dmuthesis
 xelatex -no-pdf --interaction=nonstopmode dmuthesis
 xelatex --interaction=nonstopmode dmuthesis
+del *.aux *.lo? *.toc *.ind *.inx *.gls *.glo *.gz *.ist *.idx *.ilg *.out *.bak *.bbl *.brf *.blg *.dvi *.ps *.xdv body\*.aux back\*.aux front\*.aux
+
